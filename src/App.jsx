10dd8df12f68bc8b3e6d59adae3c924ae14830a1
Karla Elizabeth <elizabeth.lala99@gmail.com>
2022-07-21 01:49:48 -0500

    feat:my homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useTranslation } from 'react-i18next';
 
 function App() {
   const [count, setCount] = useState(0)
+  const { t } = useTranslation();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='photo'>
+      <img className="image" src="./img/me.jpeg" alt="perfil"></img>
     </div>
+    <div className='name'>
+      <h1>{t("name")}</h1>
+    </div>
+    <div className='information'>
+      <p>{t("information1")}</p>
+      <p>{t("information2")}</p>
+    </div>
+    <div className='button'>
+      <div className="btn-group">
+        <button onClick={() => setCount((count) => count + 1)}>{t("button1")} </button>
+      </div>
+      <div>
+        <p>{t("button2")} {count}</p>
+      </div>
+    </div>
+    <div className='tecnologies'>
+      <p>{t("tecnologies")}</p>
+      <img className="picture" src="./img/redes.png" alt="redes"></img>
+    </div>
+  </div>
   )
 }
 
